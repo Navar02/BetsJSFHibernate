4b4737203474d38559baec84f99a97bc9b3af648
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-12-13 11:13:05 +0100

    camnios user

--- BetsJSF/src/main/java/domain/User.java
+++ BetsJSF/src/main/java/domain/User.java
@@ -8,12 +8,28 @@ import javax.persistence.Id;
 @Entity
 public class User {
 	@Id
-	String usuario;
-	String password;
+	private String usuario;
+	private String password;
 	
 	public User (String usuario,String password) {
 		this.usuario=usuario;
 		this.password=password;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	
