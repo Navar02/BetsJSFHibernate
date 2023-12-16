ed7654d206c8fc40af4755142fdc47a0ca37fcc0
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-12-16 12:09:22 +0100

    Front-end planteado (me he tenido que leer 6 manuales de la página de primefaces todo porque me gustaba el aspecto de la tabla, si es que soy bobo)

--- BetsJSF/src/main/java/modelo/bean/QueryQuestionBean.java
+++ BetsJSF/src/main/java/modelo/bean/QueryQuestionBean.java
@@ -2,11 +2,15 @@ package modelo.bean;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+
+import org.primefaces.event.SelectEvent;
 
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 import domain.Event;
+import domain.Question;
 
 public class QueryQuestionBean {
 	public Date dateOE;
@@ -14,6 +18,8 @@ public class QueryQuestionBean {
 	public List<domain.Question> listQ;
 	public String event;
 	public String Question;
+	public List<Event> events=new ArrayList<Event>();
+	public List<Question> questions=new ArrayList<Question>();
 	public BLFacade bl=new BLFacadeImplementation();
 	
 	public Date getFecha() {
@@ -24,8 +30,33 @@ public class QueryQuestionBean {
 		this.dateOE = dateOE;
 	}
 	
-	public List<Event> getEvents(Date dateOE){
-		listEv=new ArrayList<Event>(bl.getEvents(dateOE));
-		return listEv;
+	public void setEvents(Date d){
+		events.clear();
+
+		List<Event> eventsVector = bl.getEvents(d);
+		if(eventsVector.size()<0) {
+
+		}else {
+			Iterator<Event> i= eventsVector.iterator();
+			while(i.hasNext()) {
+				events.add(i.next());
+			}
+		}
+	}
+	
+	public void onDateSelect(SelectEvent event) {
+
+		setEvents((Date)event.getObject());
+	}
+	
+	public void onRowSelect(SelectEvent eve) {
+		Event even=(Event)eve.getObject();
+		setQuestions((Event) eve.getObject());
+	}
+
+	private void setQuestions(Event object) {
+
+		this.questions = questions;
+
 	}
 }
